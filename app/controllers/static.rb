88f072a1afc2d20f86e209d01f8ad5ee5c937811
Jian <jianhew@gmail.com>
2016-04-18 21:24:13 +0800

    first commit

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,16 @@
 get '/' do
-  erb :"static/index"
+  	erb :"static/index"
+end
+
+post '/urls' do
+	@url = Url.create(long_url: params[:long_url])
+	@url.shorten
+	@url.save
+	erb :"static/index"
+end
+
+
+get '/:short_url' do
+  url = Url.find_by(short_url: params[:short_url])
+  redirect "http://" + url.long_url
 end
---
+++ app/models/url.rb
@@ -0,0 +1,5 @@
+class Url < ActiveRecord::Base
+	def shorten
+    self.short_url = ([*('A'..'Z'),*('0'..'9')]-%w(0 1 I O)).sample(8).join
+  end
+end
